98e2848f3d7ebb40d5b5be00ac6925448fe7f8da
zeynep yalınız <zeynepyaliniz7@gmail.com>
2021-12-31 19:59:29 +0300

    rabbitmq

---
+++ RabbitMq.Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMq.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://ftiplzny:HjkfuxDy_Tpu-z-Qho4WwU5pn9GBcdgN@fish.rmq.cloudamqp.com/ftiplzny");
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("hello-queue",true,false,false);
+
+            string message = "hello world";
+            var messageBody = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(string.Empty,"hello-queue",null,messageBody);
+            Console.WriteLine("Message Sent!!!!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMq.Subscriber/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMq.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://ftiplzny:HjkfuxDy_Tpu-z-Qho4WwU5pn9GBcdgN@fish.rmq.cloudamqp.com/ftiplzny");
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            //channel.QueueDeclare("hello-queue", true, false, false);
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume("hello-queue",true,consumer);
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine("Message: {0}",message);
+                
+
+            };
+            Console.ReadLine();
+        }
+
+        //private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
